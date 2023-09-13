c65ea9310bc022b0d3c5110836ae58ac9e6d8713
Excel Duncan <excelduncan12@gmail.com>
2023-09-13 15:09:51 +0800

    Adding 4 new standalone files for the header, footer, content, and navbar in hopes of making the html file cleaner.

---
+++ chapters.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Moonxel - Chapters</title>
+  <!-- Add your CSS stylesheets and external dependencies here -->
+  <link rel="stylesheet" href="styles.css">
+  <script src="navbar.js"></script> <!-- Include the JavaScript file -->
+</head>
+<body>
+  <!-- Include the header -->
+  <?php include 'header.php'; ?>
+
+  <!-- Placeholder for the dynamically loaded navigation bar -->
+  <div id="navbar-placeholder"></div>
+
+  <!-- Content area -->
+  <main>
+    <h1>Chapters</h1>
+    <p>Here you can find the chapters of your webnovel.</p>
+  </main>
+
+  <!-- Include the footer -->
+  <?php include 'footer.php'; ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,3 @@
+<header>
+    <!-- Your header content goes here -->
+</header>
